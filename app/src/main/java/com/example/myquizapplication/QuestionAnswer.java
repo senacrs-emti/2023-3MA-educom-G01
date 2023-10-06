41aa24766985e05d21d8fee05d6f2c1c8405fdcb
iprehandai <yanaluiza26y@gmail.com>
2023-10-06 12:27:39 -0300

    0610

--- app/src/main/java/com/example/myquizapplication/QuestionAnswer.java
+++ app/src/main/java/com/example/myquizapplication/QuestionAnswer.java
@@ -3,11 +3,13 @@ package com.example.myquizapplication;
 public class QuestionAnswer {
 
     public static String question[] = {
-
+        "Qual é a cor do céu?",
+            "Qual é a primeira letra do alfabeto?",
+            "Da onde vem os bebes?"
     };
 
     public static String choices[][] = {
-
+            {"Preto","Amarelo","Azul","Vermelho"}
     };
 
     public static String correctAnswers[] = {
