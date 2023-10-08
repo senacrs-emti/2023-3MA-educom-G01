ec0d01fe5681ade3d1aae4c40fc4f63c644d27b3
Ana Luiza <yanaluiza26y@gmail.com>
2023-10-08 20:28:08 -0300

    Formulação das questions

--- app/src/main/java/com/example/myquizapplication/MainActivity.java
+++ app/src/main/java/com/example/myquizapplication/MainActivity.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     TextView TotalQuestionsTextView;
     TextView questionTextView;
     Button ansA, ansB, ansC, ansD;
     Button submitBtn;
+
+    ihnt score = 0;
+    int totalQuestions = QuestionAnswer.question.length;
+    int cuurentQuestionIndex = 0;
+    String selectedAnswer = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,49 @@ public class MainActivity extends AppCompatActivity {
         ansB = findViewById(R.id.ans_B);
         ansC = findViewById(R.id.ans_C);
         ansD = findViewById(R.id.ans_D);
+        submitBtn = findViewById(R.id.submit_btn);
+
+        ansA.setOnClickListener(this);
+        ansB.setOnClickListener(this);
+        ansC.setOnClickListener(this);
+        ansD.setOnClickListener(this);
+        submitBtn.setOnClickListener(this);
+
+        totalQuestionsTextView.setText("Total questions: " + totalQuestions);
+
+        loadNewQuestion();
 
     }
+
+    @Override
+    public void onClick(View view){
+
+        ansA.setBackgroundColor(Color.White);
+        ansB.setBackgroundColor(Color.White);
+        ansC.setBackgroundColor(Color.White);
+        ansD.setBackgroundColor(Color.White);
+
+        Button clickedButton = (Button) view;
+        if(clickedButton.getId() == R.id.submit_btn){
+            cuurentQuestionIndex++;
+            loadNewQuestion();
+            if(selectedAnswer.equals(QuestionAnswer.correctAnswers[currentQuestionIndex])){
+                score++;
+            }
+
+        }else{
+            selectedAnswer = clickedButton.getText().toString();
+            clickedButton.setBackgroundColor(Color.MAGENTA);
+        }
+
+    }
+
+    void loadNewQuestion(){
+        questionTextView.setTextView(QuestionAnswer.question[curentQuestionIndex]);
+        ansA.setTec(QuestionAnswer.choices[currentQuestionIndex][0]);
+        ansB.setTec(QuestionAnswer.choices[currentQuestionIndex][1]);
+        ansC.setTec(QuestionAnswer.choices[currentQuestionIndex][2]);
+        ansD.setTec(QuestionAnswer.choices[currentQuestionIndex][3]);
+    }
+
 }
