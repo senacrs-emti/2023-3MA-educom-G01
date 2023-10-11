5a32fea17bb519226546e97f00c64a63fe810a06
IPreHandaI <yanaluiza26y@gmail.com>
2023-10-11 11:19:00 -0300

    Quiz Certinho

--- app/src/main/java/com/example/myquizapplication/QuestionAnswer.java
+++ app/src/main/java/com/example/myquizapplication/QuestionAnswer.java
@@ -3,21 +3,36 @@ package com.example.myquizapplication;
 public class QuestionAnswer {
 
     public static String question[] = {
-        "Qual é a cor do céu?",
-        "Qual é a primeira letra do alfabeto?",
-        "Da onde vem os bebes?"
+        "Quanto é 2 + 2?",
+        "Quanto é 37 + 25?",
+        "Quanto é 6 - 3?",
+        "Quanto é 57 - 17?",
+        "Quanto é 3 * 2?",
+        "Quanto é 2 * 72?",
+        "Quanto é 6 / 2?",
+        "Quanto é 24 / 3?"
     };
 
     public static String choices[][] = {
-        {"Preto","Amarelo","Azul","Vermelho"},
-        {"B", "D", "P", "A"},
-        {"Relação Sexual", "Cegonha", "Sedex", "Da Plantinha"}
+        {"3", "4", "8", "6"},
+        {"62", "76", "65", "83"},
+        {"4", "2", "5", "3"},
+        {"40", "45", "27", "33"},
+        {"8", "5", "6", "7"},
+        {"152", "138", "144", "150"},
+        {"3", "4", "2", "6"},
+        {"4", "6", "5", "8"}
     };
 
     public static String correctAnswers[] = {
-        "Azul",
-        "A",
-        "Relação Sexual"
+        "4",
+        "62",
+        "3",
+        "40",
+        "6",
+        "144",
+        "3",
+        "8"
     };
 
 }
