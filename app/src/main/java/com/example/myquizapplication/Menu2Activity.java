84fdc991c6261884c300cfb6553a97f4869103ed
Valmir de Oliveira Filho <mirinho3789@gmail.com>
2023-10-16 08:05:52 -0300

    commit implementação operações menu

---
+++ app/src/main/java/com/example/myquizapplication/Menu2Activity.java
@@ -0,0 +1,14 @@
+package com.example.myquizapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Menu2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu2);
+    }
+}
