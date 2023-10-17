fad1baa1eca451cb331ef89362c5d0ccae494afc
IPreHandaI <yanaluiza26y@gmail.com>
2023-10-17 08:56:04 -0300

    Revert "Quiz Certinho"
    This reverts commit 5a32fea17bb519226546e97f00c64a63fe810a06.

--- app/src/main/java/com/example/myquizapplication/QuestionAnswer.java
+++ app/src/main/java/com/example/myquizapplication/QuestionAnswer.java
@@ -3,36 +3,21 @@ package com.example.myquizapplication;
 public class QuestionAnswer {
 
     public static String question[] = {
-        "Quanto é 2 + 2?",
-        "Quanto é 37 + 25?",
-        "Quanto é 6 - 3?",
-        "Quanto é 57 - 17?",
-        "Quanto é 3 * 2?",
-        "Quanto é 2 * 72?",
-        "Quanto é 6 / 2?",
-        "Quanto é 24 / 3?"
+        "Qual é a cor do céu?",
+        "Qual é a primeira letra do alfabeto?",
+        "Da onde vem os bebes?"
     };
 
     public static String choices[][] = {
-        {"3", "4", "8", "6"},
-        {"62", "76", "65", "83"},
-        {"4", "2", "5", "3"},
-        {"40", "45", "27", "33"},
-        {"8", "5", "6", "7"},
-        {"152", "138", "144", "150"},
-        {"3", "4", "2", "6"},
-        {"4", "6", "5", "8"}
+        {"Preto","Amarelo","Azul","Vermelho"},
+        {"B", "D", "P", "A"},
+        {"Relação Sexual", "Cegonha", "Sedex", "Da Plantinha"}
     };
 
     public static String correctAnswers[] = {
-        "4",
-        "62",
-        "3",
-        "40",
-        "6",
-        "144",
-        "3",
-        "8"
+        "Azul",
+        "A",
+        "Relação Sexual"
     };
 
 }
