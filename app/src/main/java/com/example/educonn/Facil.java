a2039f08de774f07d3ce9a12ef62887f3d86dac3
IPreHandaI <yanaluiza26y@gmail.com>
2023-11-06 11:10:59 -0300

    facil comeco

--- app/src/main/java/com/example/educonn/Facil.java
+++ app/src/main/java/com/example/educonn/Facil.java
@@ -1,13 +1,18 @@
 package com.example.educonn;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class Facil extends AppCompatActivity {
+public class Facil extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_facil);
+
     }
-}
+
