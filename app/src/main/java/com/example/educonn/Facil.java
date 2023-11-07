9579988655c5880c71dcae7f2c9acd2b8d833942
IPreHandaI <yanaluiza26y@gmail.com>
2023-11-07 09:28:54 -0300

    bug das questions

--- app/src/main/java/com/example/educonn/Facil.java
+++ app/src/main/java/com/example/educonn/Facil.java
@@ -2,9 +2,9 @@ package com.example.educonn;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
