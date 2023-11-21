74f017229657735201fb6656ade1bfa9fe1b167e
VALMIR-DE-OLIVEIRA-FILHO <mirinho3789@gmail.com>
2023-11-21 09:18:12 -0300

    commit final
    correções de design e acessibilidade

--- app/src/main/java/com/example/educonn/Final.java
+++ app/src/main/java/com/example/educonn/Final.java
@@ -1,7 +1,10 @@
 package com.example.educonn;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Final extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class Final extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final);
+
+
+        Button btnRecomeca;
+
+        btnRecomeca = findViewById(R.id.btnRecomeca);
+        btnRecomeca.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent  MainActivity = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(MainActivity);
+            }
+        });
+
     }
 }
